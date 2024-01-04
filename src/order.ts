788b443b990de851fb4135927b4ec58afca24218
Erik Barroso <coderik1996@gmail.com>
2024-01-04 18:05:10 -0300

    class order, we still need to separate the responsibilities

---
+++ src/order.ts
@@ -0,0 +1,34 @@
+import Item from "./items";
+
+export default class Order {
+  code: string;
+  items: Item[];
+  constructor() {
+    this.code = `${Math.floor(Math.random() * 100000)}`;
+    this.items = [];
+  }
+
+  addItem(item: Item) {
+    this.items.push(item);
+  }
+
+  getSubtotal() {
+    let total = 0;
+    for (const item of this.items) {
+      total += item.price;
+    }
+    return total;
+  }
+
+  getTaxes() {
+    let taxes = 0;
+    for (const item of this.items) {
+      taxes += item.calculateTaxes();
+    }
+    return taxes;
+  }
+
+  getTotal() {
+    return this.getSubtotal() + this.getTaxes();
+  }
+}
